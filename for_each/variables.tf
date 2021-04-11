396f3cd25979587329e218e2772f67fe7be834b9
DeeLaptop <deeptesh@laptop.com>
2021-04-11 13:37:35 +0530

    modified for_each variables and output variables when you run for each

--- for_each/variables.tf
+++ for_each/variables.tf
@@ -3,13 +3,6 @@ variable "user_names" {
   type        = list(string)
   default     = ["neo", "trinity", "morpheus"]
 }
-
-output "neo_arn" {
-  value       = aws_iam_user.example[0].arn
-  description = "The ARN for user Neo"
-}
-
-output "all_arns" {
-  value       = aws_iam_user.example[*].arn
-  description = "The ARNs for all users"
+output "all_users" {
+  value = aws_iam_user.example
 }
